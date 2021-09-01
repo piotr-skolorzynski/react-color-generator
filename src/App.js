266372df9a116a7570af490235f7f8d168943dad
Piotr <p.skolorzynski@gmail.com>
2021-09-01 08:31:50 +0200

    modify App.js file modify App component add basic structure of component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,30 @@ import SingleColor from './SingleColor'
 import Values from 'values.js' //biblioteka zassana poprzez yarn add values.js
 
 function App() {
-  return <h2>color generator project</h2>
+
+  const [color, setColor] = useState('');
+  const [isError, setIsError] = useState(false);
+  const [list, setList] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    console.log('submit');
+  }
+
+  return (
+    <>
+    <section className="container">
+        <h3>color generator</h3>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={color} placeholder="#f15025" onChange={e => setColor(e.target.value)} />
+          <button type="submit" className="btn">generate</button>
+        </form>
+    </section>
+    <section className="colors">
+      <h4>list goes here</h4>
+    </section>
+    </>
+  );
 }
 
 export default App;
